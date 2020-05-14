a7f7d0dd7886dcebd75cef09f0b7988faa2d32ef
Adrian Krystkiewicz <adriankrystkiewicz@gmail.com>
2020-05-14 16:43:28 +0200

    Updates

---
+++ precache-manifest.60ff8b695eeec6e4449c155852cc4a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accd57ab8a7d27f73cf35497f14eee38",
+    "url": "/rot13/index.html"
+  },
+  {
+    "revision": "4b0d6c95f92c6720a276",
+    "url": "/rot13/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8afe18d4342aea74b6e9",
+    "url": "/rot13/static/js/2.b6e5b357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rot13/static/js/2.b6e5b357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0d6c95f92c6720a276",
+    "url": "/rot13/static/js/main.5f7e02b1.chunk.js"
+  },
+  {
+    "revision": "1bd00985150c960dd7fc",
+    "url": "/rot13/static/js/runtime-main.5f750b86.js"
+  }
+]);
